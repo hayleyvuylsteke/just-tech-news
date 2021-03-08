dc266e0e48e20690a0e6bf9740eb8afde4b885f8
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-08 18:32:37 -0500

    new files

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-const { User } = require('.');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
@@ -7,28 +6,31 @@ class Comment extends Model {}
 Comment.init(
   {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     comment_text: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'User',
-            key: 'id'
-        },
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
     post_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'Post',
-            key: 'id'
-        }
-    }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
     }
   },
   {
